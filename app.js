b53e893e1ed7f460dc9e5fde467b96ba2436fa95
李游城 <geeklyc@163.com>
2020-05-06 22:53:45 +0800

    【新增】websocket了解

--- app.js
+++ app.js
@@ -1,25 +1,3 @@
-// const Koa = require('koa');
-
-// const bodyParser = require('koa-bodyparser');
-
-// const controller = require('./controller');
-
-// const app = new Koa();
-
-// // log request URL:
-// app.use(async (ctx, next) => {
-//     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
-//     await next();
-// });
-
-// // parse request body:
-// app.use(bodyParser());
-
-// // add controllers:
-// app.use(controller());
-
-// app.listen(3000);
-// console.log('app started at port 3000...');
 const Koa = require('koa');
 
 const bodyParser = require('koa-bodyparser');
@@ -61,5 +39,52 @@ app.use(templating('views', {
 // add controller:
 app.use(controller());
 
-app.listen(3000);
-console.log('app started at port 3000...');
+module.exports = app;
+
+// const WebSocket = require('ws');
+
+// const WebSocketServer = WebSocket.Server;
+
+// const wss = new WebSocketServer({
+//     port: 3000
+// });
+
+// wss.on('connection', function (ws) {
+//     console.log(`[SERVER] connection()`);
+//     ws.on('message', function (message) {
+//         console.log(`[SERVER] Received: ${message}`);
+//         setTimeout(() => {
+//             ws.send(`What's your name?`, (err) => {
+//                 if (err) {
+//                     console.log(`[SERVER] error: ${err}`);
+//                 }
+//             });
+//         }, 1000);
+//     })
+// });
+
+// console.log('ws server started at port 3000...');
+
+// // client test:
+
+// let count = 0;
+
+// let ws = new WebSocket('ws://localhost:3000/ws/chat');
+
+// ws.on('open', function () {
+//     console.log(`[CLIENT] open()`);
+//     ws.send('Hello!');
+// });
+
+// ws.on('message', function (message) {
+//     console.log(`[CLIENT] Received: ${message}`);
+//     count++;
+//     if (count > 3) {
+//         ws.send('Goodbye!');
+//         ws.close();
+//     } else {
+//         setTimeout(() => {
+//             ws.send(`Hello, I'm Mr No.${count}!`);
+//         }, 1000);
+//     }
+// })
